822eb43e05185500ff0fa6c263232288292d2333
Yugai Denis <yugai_denis@mail.ru>
2022-11-08 12:25:08 +0600

    Выполнение задания 3

--- task_3/script.js
+++ task_3/script.js
@@ -1,1 +1,9 @@
-﻿
+﻿const container = document.getElementById('container');
+console.log(container);
+
+for (i=0; i < 5; i++) {
+    const newElement = document.createElement('div');
+    newElement.innerText = `Element ${i+1}`;
+    newElement.className = "element"
+    container.appendChild(newElement)
+}
