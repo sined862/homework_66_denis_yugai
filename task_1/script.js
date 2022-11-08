e89b091a638a40f52cfeb6d0f7376764a2c6ad9c
Yugai Denis <yugai_denis@mail.ru>
2022-11-08 11:52:53 +0600

    Создание index.html + подключение script.js к файлу index.html + Выполнение задания 1

---
+++ task_1/script.js
@@ -0,0 +1,13 @@
+﻿const stringOne = document.getElementById("string-1").textContent;
+const stringTwo = document.getElementById("string-2").textContent;
+const stringThree = document.getElementById("string-3").textContent;
+const stringFour = document.getElementById("string-4").textContent;
+const stringFive = document.getElementById("string-5").textContent;
+const stringSix = document.getElementById("string-6").textContent;
+
+console.log(stringThree);
+console.log(stringFive);
+console.log(stringTwo);
+console.log(stringSix);
+console.log(stringFive);
+console.log(stringOne);
