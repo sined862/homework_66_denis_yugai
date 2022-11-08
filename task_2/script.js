953b33a99dedde5417a72b2b7f73060d5af23d06
Yugai Denis <yugai_denis@mail.ru>
2022-11-08 12:01:48 +0600

    Выполнение залания 2(Создание index.html + подключение файла js)

---
+++ task_2/script.js
@@ -0,0 +1,8 @@
+﻿const elements = document.getElementsByClassName('element');
+
+elements[0].style.color = 'red';
+elements[1].style.color = 'red';
+elements[2].style.color = 'red';
+elements[3].style.color = 'green';
+elements[4].style.color = 'green';
+elements[5].style.color = 'green';
