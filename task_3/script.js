8275e84d67c8248e82302e3c0c552a611f8abf39
Yugai Denis <yugai_denis@mail.ru>
2022-11-08 13:17:21 +0600

    Добавление ; ко всем строкам js скрипта 3-го задания

--- task_3/script.js
+++ task_3/script.js
@@ -6,14 +6,14 @@ console.log(container);
 function elementsAdd(number) {
     for (i=0; i < number; i++) {
         const elementDiv = document.createElement('div');
-        const elementSpan = document.createElement('span')
-        elementSpan.innerText = ` ${i+1}`
+        const elementSpan = document.createElement('span');
+        elementSpan.innerText = ` ${i+1}`;
         elementDiv.innerText = `Element`;
-        elementDiv.className = "element"
-        elementSpan.className = `color-${i+1}`
-        container.appendChild(elementDiv)
-        elementDiv.appendChild(elementSpan)  
+        elementDiv.className = "element";
+        elementSpan.className = `color-${i+1}`;
+        container.appendChild(elementDiv);
+        elementDiv.appendChild(elementSpan);
     }
 }
 
-elementsAdd(5)
+elementsAdd(5);
